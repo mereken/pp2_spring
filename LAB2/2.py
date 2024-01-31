cf926a5f58d5666ae951831b513b829616c92450
Mereke Nuray <155009093+mereken@users.noreply.github.com>
2024-02-01 00:08:43 +0600

    Add files via upload

---
+++ LAB2/2.py
@@ -0,0 +1,104 @@
+print(10 + 5)
+
+x = 5
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+x = 5
+print(x)
+
+x = 5
+x += 3
+print(x)
+
+x = 5
+x -= 3
+print(x)
+
+x = 5
+x *= 3
+print(x)
+
+x = 5
+x /= 3
+print(x)
+
+x = 5
+x %= 3
+print(x)
+
+x = 5
+y = 3
+print(x == y)
+
+x = 5
+y = 3
+print(x != y)
+
+x = 5
+y = 3
+print(x > y)
+
+x = 5
+y = 3
+print(x >= y)
+
+x = 5
+print(x > 3 and x < 10)
+
+x = 5
+print(x > 3 or x < 4)
+
+x = 5
+print(x > 5 and x < 10)
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is not z)
+print(x is not y)
+print(x != y)
+
+x = ["apple", "banana"]
+print("banana" in x)
+
+x = ["apple", "banana"]
+print("pineapple" not in x)
+
+print(6 & 3)
+print(6 | 3)
+print(6 ^ 3)
+print(~3)
+print(3 << 2)
+print(3 >> 2)
+
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
+print(100 + ~3)
+print(100 + 5 * 3)
+print(100 - 5 * 3)
+print(8 >> 4 - 2)
+print(6 & 2 + 1)
+print(6 ^ 2 + 1)
+print(6 | 2 + 1)
+print(5 == 4 + 1)
+print(not 5 == 5)
+print(1 or 2 and 3)
+print(4 or 5 + 10 or 8)
+print(5 + 4 - 7 + 3)
+
+
+
